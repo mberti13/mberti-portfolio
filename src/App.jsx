ecdc506c31ed99add44e88132e9da19982bd3a40
MJB <matt.berti13@gmail.com>
2022-08-05 12:48:43 -0500

    styled nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 import Nav from './components/Nav'
+import Header from "./components/Header";
 
 
+  
 const App = () => {
     return (
-        <div>Hello world!</div>
+        <div>
+            <Nav />
+            <Header />
+        </div>
     )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li>About Me</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
