fe20a8a22182776c651643488c6ebcba46fdb569
MJB <matt.berti13@gmail.com>
2022-08-06 18:57:59 -0500

    Revert "added styling to about"
    This reverts commit e0034afe929950488d046218af9d037a2331faf1.
    
    reverted to about stylings

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom';
-
 const Nav = () => {
   return (
     <nav className='sticky-nav'>
         <ul>
-          <li><Link to={"/"}>Home</Link></li>
-            <li><Link to={"/about"}>About Me</Link></li>
-            <li className='resume-link'><a href="https://docs.google.com/document/d/1x1TdlubHrWFR0pQXBxFbFX2pLXvMj8u7pe4RPQKmK1s/edit?usp=sharing"></a>Resume</li>
+            <li className='nav-container'>About Me</li>
+            <li className='resume-nav nav-list-item'
+            href='/'
+            
+            >Resume</li>
             <li>Projects</li>
             <li>Contact</li>
         </ul>
