1b808d71cc2ef7ea88fe8d1969e9b2aacbe50d45
MJB <matt.berti13@gmail.com>
2022-08-06 16:21:47 -0500

    added text to about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+
 import Nav from './components/Nav'
 import Header from "./components/Header";
+import About from "./components/About";
 
 
   
 const App = () => {
     return (
+        <Router>
         <div>
             <Nav />
-            <Header />
-            <div className="container">
+            <Routes>
+                <Route path="/" element={<Header />}></Route>
+                <Route path="/about" element={<About />}></Route>
+            </Routes>
+            <div className="container-border">
                 <div className="lines">
                     <div className="circle"></div>
                 </div>
             </div>
         </div>
+        </Router>
     )
 }
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>
-        
+    <div className="about-page">
+      <div className="about-content">
+        <h1>About Me:</h1>
+        <p>
+          I am a Fullstack Web Developer with a B.B.A. in Marketing from Texas
+          Tech University, with experience in Sales, Marketing, and Management.
+          I am looking to develop projects, on both the front and back-end for
+          various utilities. Including, but not limited to: UI/UX Design,
+          Blockchain Development and Software Development. I am 22 years old
+          currently living in St. Louis, MO, with previous experience in the
+          Business/Marketing/Management Industries, as well as MERN Stack Development. My overall goal is to build
+          apps, websites and programs that can truly make a difference. Scroll
+          down for some of my recent projects and my contact info!
+        </p>
+      </div>
+      
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
