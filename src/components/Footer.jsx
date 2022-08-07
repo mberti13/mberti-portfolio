b4b5c259eb5e9a6599490f02b8041224a586dcda
MJB <matt.berti13@gmail.com>
2022-08-07 00:36:10 -0500

    added links to footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
   return (
     <footer>
         <div className="footer-icons">
-            <a className="icon fa-2x"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
-            <a className="icon fa-2x"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+            <a className="icon fa-2x" href="https://www.linkedin.com/in/matthew-berti/" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+            <a className="icon fa-2x" href="https://github.com/mberti13" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
         </div>
     </footer>
   )
