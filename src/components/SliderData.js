2aad3cef98023113c1d99fe2f83cb80bb3dc3f0e
MJB <matt.berti13@gmail.com>
2022-09-24 17:15:37 -0500

    added slider data to components folder

---
+++ src/components/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+    {
+        image: "../assets/images/Crescendo.jpg",
+        alt: "Crescendo, Full-stack project"
+    },
+    {
+        image: "../assets/images/donate homepage.png",
+        alt: "Donate, a MERN stack project for displaying charities worth donating to"
+    },
+    {
+        image: "../assets/images/password-generator-photo.jpg",
+        alt: "A frontend designed and functional password generator"
+    },
+    {
+        image: "../assets/images/Screenshot 2022-03-27 224334.jpg",
+        alt: "Super Hero Wiki, designed with Marvel API to provide results for user's searches"
+    },
+    {
+        image: '../assets/images/react-calculator.png',
+        alt: "A calculator built using React and Javascript"
+    }
+]
