08200f145e339db53a79b3aa9ea2087abf9b4fd6
MJB <matt.berti13@gmail.com>
2022-09-27 19:23:11 -0500

    redid paths

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
                 <Route path="/about" element={<About />}></Route>
                 <Route path="/contact" element={<Contact />}></Route>
                 <Route path="/projects" element={<Projects slides={SliderData} />}></Route>
-                <Route path="/modal" element={<Modal />}></Route>
+                {/* <Route path="/modal" element={<Modal />}></Route> */}
             </Routes>
             <Footer></Footer>
         </div>
